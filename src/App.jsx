b2b66fb42c6713451f048ea3f37d0d3b25348dda
Chhayank <cdixit1313@gmail.com>
2025-09-22 22:37:29 +0530

    implemented xmodal

---
+++ src/App.jsx
@@ -0,0 +1,79 @@
+import React, { useState } from "react";
+import "./App.css";
+import Modal from "./components/Modal";
+
+const App = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [phone, setPhone] = useState(null);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (phone.toString().length !== 10) {
+      alert("Invalid phone number. Please enter a 10-digit phone number.");
+    }
+  };
+
+  return (
+    <div className="container">
+      <h1>User Details Modal</h1>
+      <button
+        style={{
+          color: "white",
+          backgroundColor: "blue",
+          width: "150px",
+          height: "50px",
+          border: "none",
+          borderRadius: "10px",
+          cursor: "pointer",
+        }}
+        onClick={() => {
+          setIsOpen(true);
+        }}
+      >
+        Open Form
+      </button>
+
+      <Modal
+        isOpen={isOpen}
+        setIsOpen={setIsOpen}
+        onclose={() => setIsOpen(false)}
+      >
+        <div className="modal">
+          <h1>Fill Details</h1>
+          <form className="form" onSubmit={handleSubmit}>
+            <label htmlFor="username">Username:</label>
+            <input id="username" type="text" />
+            <label htmlFor="email">Email:</label>
+            <input id="email" type="email" />
+            <label htmlFor="phone">Phone Number:</label>
+            <input
+              id="phone"
+              type="number"
+              value={phone}
+              onChange={(e) => setPhone(e.target.value)}
+            />
+            <label htmlFor="dob">Date of Birth:</label>
+            <input id="dob" type="date" />
+
+            <button
+              style={{
+                color: "white",
+                backgroundColor: "blue",
+                width: "150px",
+                height: "50px",
+                border: "none",
+                borderRadius: "10px",
+                cursor: "pointer",
+              }}
+              type="submit"
+            >
+              Submit
+            </button>
+          </form>
+        </div>
+      </Modal>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/Modal.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import ReactModal from "react-modal";
+
+ReactModal.setAppElement("#root");
+
+const Modal = ({ children, isOpen, onclose }) => {
+  return (
+    <ReactModal
+      isOpen={isOpen}
+      onRequestClose={onclose}
+      style={{
+        overlay: {
+          backgroundColor: "rgba(0, 0, 0, 0.5)",
+        },
+        content: {
+          top: "50%",
+          left: "50%",
+          right: "auto",
+          bottom: "auto",
+          marginRight: "-50%",
+          transform: "translate(-50%, -50%)",
+          borderRadius: "10px",
+          padding: "20px",
+        },
+      }}
+    >
+      <div className="modal-content">{children}</div>
+    </ReactModal>
+  );
+};
+
+export default Modal;
